5897a30b9eeb87756047fe479a6c414451a5d925
SeeEndX <ceh938of@yandex.ru>
2023-03-26 12:24:18 +0800

    Поворот модели почти готов + плавность поворота + поворот по оси y (пока не полный)

--- app/src/main/java/mxkmn/blindy/MainActivity.kt
+++ app/src/main/java/mxkmn/blindy/MainActivity.kt
@@ -5,13 +5,19 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+import dev.romainguy.kotlin.math.Float3
 import io.github.sceneview.ar.ArSceneView
 import io.github.sceneview.ar.getDescription
 import io.github.sceneview.ar.node.ArModelNode
 import io.github.sceneview.ar.node.PlacementMode
 import io.github.sceneview.math.Position
+import io.github.sceneview.math.Rotation
 import io.github.sceneview.utils.setFullScreen
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.math.atan2
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     lateinit var sceneView: ArSceneView
@@ -41,12 +47,34 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         placeModelButton = findViewById<ExtendedFloatingActionButton>(R.id.placeModelButton).apply {
             setOnClickListener {
                 modelNode?.anchor()
-                placeModelButton.isVisible = false
-                sceneView.planeRenderer.isVisible = false
+                placeModelButton.isVisible = true
+                sceneView.planeRenderer.isVisible = true
             }
         }
 
         setupModelNode()
+
+        // пересчет поворота баннера
+        lifecycleScope.launch {
+            while (true) {
+                rotateModel()
+                delay(70)
+            }
+        }
+    }
+
+    // поворот модели относительно камеры
+    fun rotateModel(){
+        val cameraPosition = sceneView.cameraNode.worldPosition.toFloatArray().let { arr ->
+            Float3(arr[0], arr[1], arr[2])
+        }
+        val modelPosition = modelNode?.worldPosition?.toFloatArray().let { arr ->
+            Float3(arr?.get(0) ?: 0f, arr?.get(1) ?: 0f, arr?.get(2) ?: 0f)
+        }
+        val direction = cameraPosition - modelPosition
+        val angle = Math.toDegrees(atan2(direction.y.toDouble(), direction.z.toDouble())).toFloat()
+
+        modelNode?.rotation = Rotation(0f, angle, 0f)
     }
 
     fun setupModelNode() {
@@ -59,8 +87,8 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
             applyPoseRotation = false
             loadModelGlbAsync(
                 context = this@MainActivity,
-                glbFileLocation = "models/spiderbot.glb",
-                autoAnimate = true,
+                glbFileLocation = "models/lustra.glb",
+                autoAnimate = false,
                 scaleToUnits = 1.0f,
                 // Place the model origin at the bottom center
                 centerOrigin = Position(y = -1.0f)
